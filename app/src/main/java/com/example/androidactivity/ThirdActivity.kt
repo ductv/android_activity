7370dbcbb1363a10606bea5e30c2f415b35f803f
Duc Ta Van <ductv53@gmail.com>
2023-01-05 22:13:17 +0700

    add 2 new fragments

--- app/src/main/java/com/example/androidactivity/ThirdActivity.kt
+++ app/src/main/java/com/example/androidactivity/ThirdActivity.kt
@@ -2,10 +2,27 @@ package com.example.androidactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.os.bundleOf
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class ThirdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_third)
+
+        if (savedInstanceState == null){
+            val bundle = bundleOf("some_int" to 0)
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<FirstFragment>(R.id.fragment_container_view_first, args = bundle)
+            }
+
+            val bundle2 = bundleOf("some_int" to 0)
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<SecondFragment>(R.id.fragment_container_view_second, args = bundle2)
+            }
+        }
     }
 }
