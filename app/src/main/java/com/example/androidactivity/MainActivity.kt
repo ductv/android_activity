092348643a54e14e4cbacf40bcdfb932875a1e16
Duc Ta Van <ductv53@gmail.com>
2023-01-05 21:37:16 +0700

    Change activities by using Intent, add fragment lib to build.gradle

--- app/src/main/java/com/example/androidactivity/MainActivity.kt
+++ app/src/main/java/com/example/androidactivity/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.androidactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        val btnClick = findViewById(R.id.btn_change_activity) as Button
+
+    }
+
+    fun clickButton(v: View) {
+        Toast.makeText(applicationContext, "Button click", Toast.LENGTH_LONG).show()
+        var intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/example/androidactivity/SecondActivity.kt
+++ app/src/main/java/com/example/androidactivity/SecondActivity.kt
@@ -1,11 +1,39 @@
 package com.example.androidactivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
-class SecondActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val btnBack = findViewById<Button>(R.id.btn_back)
+        val btnNext = findViewById<Button>(R.id.btn_next)
+
+        btnBack.setOnClickListener (this)
+        btnNext.setOnClickListener (this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.btn_back -> {
+                    Toast.makeText(this, "Back button Click", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+
+                R.id.btn_next ->{
+                    Toast.makeText(this, "Next button Click", Toast.LENGTH_LONG).show()
+                    val intent = Intent(this, ThirdActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 }
