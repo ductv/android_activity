edcc3d43e5baa60ff7ee6f16e026b805f2741f0f
Duc Ta Van <ductv53@gmail.com>
2023-01-08 22:15:55 +0700

    Coding for communication between 2 fragments using ViewModel

--- app/src/main/java/com/example/androidactivity/SecondActivity.kt
+++ app/src/main/java/com/example/androidactivity/SecondActivity.kt
@@ -1,11 +1,10 @@
 package com.example.androidactivity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,13 +22,11 @@ class SecondActivity : AppCompatActivity(), View.OnClickListener {
         if (v != null) {
             when (v.id) {
                 R.id.btn_back -> {
-                    Toast.makeText(this, "Back button Click", Toast.LENGTH_LONG).show()
                     val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                 }
 
                 R.id.btn_next ->{
-                    Toast.makeText(this, "Next button Click", Toast.LENGTH_LONG).show()
                     val intent = Intent(this, ThirdActivity::class.java)
                     startActivity(intent)
                 }
--- app/src/main/java/com/example/androidactivity/SecondFragment.kt
+++ app/src/main/java/com/example/androidactivity/SecondFragment.kt
@@ -1,14 +1,21 @@
 package com.example.androidactivity
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.example.androidactivity.viewmodel.SharedViewModel
 
 class SecondFragment : Fragment(), View.OnClickListener {
+    private var sharedViewModelInstance: SharedViewModel? = null
+    private var edt_data_send: EditText? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +29,31 @@ class SecondFragment : Fragment(), View.OnClickListener {
         val view = inflater.inflate(R.layout.fragment_second, container, false)
         val btnSend: Button = view.findViewById(R.id.btn_send_fr_02)
         btnSend.setOnClickListener (this)
+        edt_data_send = view.findViewById(R.id.edt_data_send)
+
+        // as soon as the button is clicked
+        // send the data to ViewModel
+        // and the Live data will take care of
+        // updating the data inside another Fragment
+        btnSend.setOnClickListener {
+            sharedViewModelInstance?.setData(edt_data_send!!.text)
+        }
+
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        // create instances of the shared view model
+        // when the activity is created
+        sharedViewModelInstance = ViewModelProviders.of(requireActivity()).get(SharedViewModel::class.java)
+
+        // observe the data inside the view model that is mutable
+        // live of type CharSequence and set the data for edit text
+        sharedViewModelInstance!!.getData().observe(viewLifecycleOwner, Observer {
+            edt_data_send!!.text = it as Editable?
+        })
     }
 
     override fun onClick(v: View?) {
